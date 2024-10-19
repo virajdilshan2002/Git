4403ce05759236880bd2f540cc879c290c292c5c
virajdilshan2002 <virajdilshan2019@gmail.com>
2024-10-19 14:00:12 +0530

    js style modification

---
+++ js/StyleModification.js
@@ -0,0 +1,8 @@
+console.log("Style Modification File Is Loaded");
+
+const card = document.querySelector(".card");
+card.style.backgroundColor = "red";
+card.style.padding = "10px";
+card.style.color = "white";
+card.style.borderRadius = "20px";
+card.style.border = "2px solid black";
