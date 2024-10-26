9a4b9b14c89ea6d95ca5e249d8656da8ff47f963
virajdilshan2002 <virajdilshan2019@gmail.com>
2024-10-26 10:59:09 +0530

    js dynamic typing

---
+++ js/DynamicTyping.js
@@ -0,0 +1,23 @@
+//Dynamic Typing
+
+let x = 10;
+console.log(typeof x);
+
+x = "Viraj Dilshan";
+console.log(typeof x);
+
+x = true;
+console.log(typeof x);
+
+x = null;
+console.log(typeof x);
+
+x = undefined;
+console.log(typeof x);
+
+x = { name: "Viraj Dilshan", age: 22 };
+console.log(typeof x);
+
+x = [10, 20, 30];
+console.log(typeof x);
+console.log(Array.isArray(x));
